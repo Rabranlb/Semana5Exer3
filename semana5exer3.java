0a07ff701ce9ff418b2efab4bef6996acf3a0887
Raphael Leal <raphaelleal990@gmail.com>
2021-09-03 02:19:11 +0000

    Atualizado

--- semana5exer3.java
+++ semana5exer3.java
@@ -1,9 +1,9 @@
 public class semana5exer3{
     public static void main(String args[]){
         for(int i = 1; i <= 5; i++){
-            for(int j = 1; j <= 5; i++){
+            for(int j = 1; j <= 5; j++){
                 if(1%2==0){
-                    System.out.print(i*j + "  ");
+                    System.out.print(i*j + " ");
                 }
                 else if(i%2!=0){
                   System.out.print(i*j + "  ");
