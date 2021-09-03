a9ea0187f9d333a2d1c2de099cda7a5978fd1e9a
Raphael Leal <raphaelleal990@gmail.com>
2021-09-03 02:06:31 +0000

    Atualizado

---
+++ semana5exer3.java
@@ -0,0 +1,16 @@
+public class semana5exer3{
+    public static void main(String args[]){
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= 5; i++){
+                if(1%2==0){
+                    System.out.print(i*j + "  ");
+                }
+                else if(i%2!=0){
+                  System.out.print(i*j + "  ");
+                }
+
+
+            }   System.out.print("\n");
+        }
+    }
+}
